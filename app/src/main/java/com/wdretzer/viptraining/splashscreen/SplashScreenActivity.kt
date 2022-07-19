a2b5b75a1cc3a716dafbce5a185bb19bd33f24d0
WDRETZER <walter_retzer@yahoo.com.br>
2022-07-19 12:13:05 -0300

    Inclusão da Tela Splash Screen do Aplicativo, incluído uma Animação para a tela inicial utilizando a biblioteca Lottie.

---
+++ app/src/main/java/com/wdretzer/viptraining/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.wdretzer.viptraining.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wdretzer.viptraining.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
